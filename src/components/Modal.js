86dacf18154f8b780613030826f817fde256dec2
Jack Kruger <contact@coolcowfacts.com>
2021-06-13 11:47:44 -0400

    updated header for post req

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -20,7 +20,7 @@ function Modal() {
                 title: newFact
             },
             headers: {
-                'Content-Type': 'text/plain;charset=utf-8',
+                'Content-Type': 'application/json',
             },
         }).then(function (response) {
             console.log(response);
